fc00a83f2a77e3ab194f6d6d4403208995129c86
MegaMan-Ofc <tiagoc8ribeiro@gmail.com>
2026-04-09 15:26:48 +0100

    test scan implemented

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -1,6 +1,7 @@
 #include <lcom/lcf.h>
 
 #include <lcom/lab3.h>
+#include "kbc.h"
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -30,10 +31,60 @@ int main(int argc, char *argv[]) {
 }
 
 int(kbd_test_scan)() {
-  /* To be completed by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  int ipc_status;
+  message msg;
+  uint8_t irq_set;
 
-  return 1;
+  if (kbc_subscribe_int(&irq_set) != 0) return 1;
+
+  uint8_t bytes[2];
+  uint8_t size = 0;
+  bool two_bytes = false;
+  bool done = false;
+
+  while (!done) {
+    if (driver_receive(ANY, &msg, &ipc_status) != 0)
+      continue;
+
+    if (is_ipc_notify(ipc_status) && _ENDPOINT_P(msg.m_source) == HARDWARE) {
+      if (msg.m_notify.interrupts & BIT(irq_set)) {
+        kbc_ih();
+
+        if (check_kbc_error()) 
+          continue;
+
+        uint8_t data = get_current_scancode();
+
+        if (data == 0xE0) {
+          two_bytes = true;
+          bytes[0] = data;
+          size = 1;
+        }
+
+        else {
+          if (two_bytes) {
+            bytes[1] = data;
+            size = 2;
+            two_bytes = false;
+          } else {
+            bytes[0] = data;
+            size = 1;
+          }
+
+          bool make = !(bytes[size - 1] & BIT(7));
+          kbd_print_scancode(make, size, bytes);
+
+          if (size == 1 && bytes[0] == 0x81)
+            done = true;
+        }
+      }
+    }
+  }
+
+  if (kbc_unsubscribe_int() != 0)
+    return 1;
+
+  return 0;
 }
 
 int(kbd_test_poll)() {
--- lab3/utils.c
+++ lab3/utils.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <lcom/lcf.h>
 
 uint8_t lsb(uint16_t bytes) { return (bytes & 0xFF); }
 
@@ -13,4 +14,12 @@ uint16_t break_from_make(uint16_t keycode)
     if (is_single_byte(keycode))
         return (keycode | (1 << 7));
     return (keycode | (1 << 15));
+}
+
+int (util_sys_inb)(int port, uint8_t *value){
+    uint32_t temp;
+    if (sys_inb(port, &temp) != OK) return 1;
+
+    *value = (uint8_t) temp;
+    return 0;
 }
