3edcd4e5c85a35a9109582f03574b76aed163d00
MegaMan-Ofc <tiagoc8ribeiro@gmail.com>
2026-04-23 15:32:55 +0100

    video.h .c and makefile

---
+++ lab5/video.c
@@ -0,0 +1,22 @@
+#include <lcom/lcf.h>
+#include "video.h"
+
+int vg_init_mode(uint16_t mode) {
+    return 0;
+}
+
+int draw_pixel(uint16_t x, uint16_t y, uint32_t color) {
+    return 0;
+}
+
+int draw_hline(uint16_t x, uint16_t y, uint16_t len, uint32_t color) {
+    return 0;
+}
+
+int draw_rectangle(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color) {
+    return 0;
+}
+
+int draw_xpm(uint8_t *map, xpm_image_t img, uint16_t x, uint16_t y) {
+    return 0;
+}
