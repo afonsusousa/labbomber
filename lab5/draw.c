79099f5a2c94abe4fa6bdf5343318dd56a2ec77d
Afonso Sousa <afonso.rocha@outlook.com>
2026-04-29 11:51:54 +0100

    lab5-pretty: combined changes

---
+++ lab5/draw.c
@@ -0,0 +1,87 @@
+#include <lcom/lcf.h>
+#include "video.h"
+
+int draw_pixel(uint16_t x, uint16_t y, uint32_t color) {
+    uint16_t    hres = get_hres();
+    uint16_t    vres = get_vres();
+    unsigned    bpp = get_bytes_per_pixel();
+    char        *vmem = get_video_mem();
+
+    if (x >= hres || y >= vres) return (1);
+
+    memcpy(vmem + (hres * y + x) * bpp, &color, bpp);
+    
+    return (0);
+}
+
+int draw_hline(uint16_t x, uint16_t y, uint16_t len, uint32_t color) {
+    uint16_t    hres = get_hres();
+    uint16_t    vres = get_vres();
+    unsigned    bpp = get_bytes_per_pixel();
+    char        *vmem = get_video_mem();
+    char        *pos;
+
+    if (x >= hres || y >= vres) return (0);
+    if (x + len > hres) len = hres - x; // clip
+
+    pos = vmem + (y * hres + x) * bpp;
+
+    for (uint16_t i = 0; i < len; i++) {
+        memcpy(pos, &color, bpp);
+        pos += bpp;
+    }
+
+    return (0);
+}
+
+int draw_rectangle(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color) {
+    uint16_t    hres = get_hres();
+    uint16_t    vres = get_vres();
+    unsigned    bpp = get_bytes_per_pixel();
+    char        *vmem = get_video_mem();
+    char        *pos;
+
+    if (x >= hres || y >= vres) return (0);
+    if (x + width > hres) width = hres - x; // clip
+    if (y + height > vres) height = vres - y;
+
+    for (uint16_t yy = 0; yy < height; yy++) {
+        pos = vmem + ((y + yy) * hres + x) * bpp;
+        for (uint16_t xx = 0; xx < width; xx++) {
+            memcpy(pos, &color, bpp);
+            pos += bpp;
+        }
+    }
+
+    return (0);
+}
+
+int draw_xpm(uint8_t *map, xpm_image_t img, uint16_t x, uint16_t y) {
+    uint16_t    hres = get_hres(), vres = get_vres();
+    unsigned    bpp = get_bytes_per_pixel();
+    char        *vmem = get_video_mem();
+    uint32_t    transparent, color;
+    uint8_t     *ptr = map;
+
+    if (map == NULL) return (1);
+
+    transparent = xpm_transparency_color(img.type);
+
+    for (uint16_t i = 0; i < img.height; i++) {
+        uint16_t draw_y = y + i;
+        if (draw_y >= vres) break;
+
+        for (uint16_t j = 0; j < img.width; j++) {
+            color = 0;
+            for (unsigned k = 0; k < bpp; k++) color |= (*ptr++) << (k * 8);
+
+            uint16_t draw_x = x + j;
+            if (draw_x >= hres) continue;
+
+            if (color != transparent)
+                memcpy(vmem + (draw_y * hres + draw_x) * bpp, &color, bpp);
+        }
+    }
+
+    return (0);
+}
--- lab5/video.c
+++ lab5/video.c
@@ -1,22 +1,40 @@
 #include <lcom/lcf.h>
 #include "video.h"
 
+int is_valid_mode(uint16_t mode)
+{
+    switch (mode) {
+        case VBE_MODE_105:
+        case VBE_MODE_110:
+        case VBE_MODE_115:
+        case VBE_MODE_14C:
+        case VBE_MODE_11A:
+            return (1);
+        default:
+            return (0);
+    }
+}
+
+reg86_t vbe_reg() {
+    reg86_t r86;
+    memset(&r86, 0, sizeof(reg86_t));
+    r86.intno = BIOS_VID_INT;
+    return (r86);
+}
+
 int vg_init_mode(uint16_t mode) {
-    return 0;
-}
+    if (!is_valid_mode(mode)) return 1;
+    
+    reg86_t reg = vbe_reg();
 
-int draw_pixel(uint16_t x, uint16_t y, uint32_t color) {
-    return 0;
-}
+    reg.ah = VBE_CALL;
+    reg.al = VBE_SET_MODE;
+    reg.bx = mode | LINEAR_FRAMEBUFR;
+    
+    if (sys_int86(&reg) != 0) {
+        printf("Error setting video mode\n");
+        return 1;
+    }
 
-int draw_hline(uint16_t x, uint16_t y, uint16_t len, uint32_t color) {
-    return 0;
-}
-
-int draw_rectangle(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color) {
-    return 0;
-}
-
-int draw_xpm(uint8_t *map, xpm_image_t img, uint16_t x, uint16_t y) {
     return 0;
 }
