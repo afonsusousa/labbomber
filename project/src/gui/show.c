5d47d0f059e71a78ed2dbfbdd97b673bfb5ab511
Afonso Sousa <afonso.rocha@outlook.com>
2026-05-05 15:40:54 +0100

    restore compilation

--- project/src/gui/show.c
+++ project/src/gui/show.c
@@ -5,6 +5,12 @@
 
 #define isspace(c) ((c) == ' ' || (c) == '\t' || (c) == '\n' || (c) == '\r' || (c) == '\f' || (c) == '\v')
 
+t_widget* widget_create_overlay(uint32_t screen_w, uint32_t screen_h, void (*on_quit)(t_widget*, void*), const char *name) {
+    t_widget *overlay = widget_create(CANVAS, 0, 0, screen_w, screen_h, name);
+    overlay->on_quit = on_quit;
+    return overlay;
+}
+
 // =============================================================================
 // Generic Callbacks
 // =============================================================================
--- project/src/widgets/overlay.c
+++
@@ -1,7 +0,0 @@
-#include "widget.h"
-
-t_widget* widget_create_overlay(uint32_t screen_w, uint32_t screen_h, void (*on_quit)(t_widget*, void*), const char *name) {
-    t_widget *overlay = widget_create(CANVAS, 0, 0, screen_w, screen_h, name);
-    overlay->on_quit = on_quit;
-    return overlay;
-}
