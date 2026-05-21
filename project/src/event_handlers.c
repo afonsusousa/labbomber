8ced6b32fdddce3afaddcc34448f21e73f898f91
MegaMan-Ofc <tiagoc8ribeiro@gmail.com>
2026-05-21 17:59:35 +0100

    feat: player spawnpoint based on click counter

--- project/src/event_handlers.c
+++ project/src/event_handlers.c
@@ -5,7 +5,7 @@
 #include "draw.h"
 #include "../lib/rtc/rtc.h"
 
-void draw_debug_overlay(hw_video_t *video, const t_gui *gui, int32_t player_x, int32_t player_y, int32_t player_board_pos_x, int32_t player_board_pos_y);
+void draw_debug_overlay(hw_video_t *video, const t_gui *gui, t_game_state game);
 
 void handle_timer(hardware_t *hw_state, t_ctx *ctx) {
     t_gui *gui = &ctx->gui;
@@ -47,7 +47,7 @@ void handle_timer(hardware_t *hw_state, t_ctx *ctx) {
         draw_mouse(&hw_state->mouse, &hw_state->video);
     }
 
-    draw_debug_overlay(&hw_state->video, gui, ctx->game.players[0].pos.x, ctx->game.players[0].pos.y, ctx->game.players[0].board_pos.x, ctx->game.players[0].board_pos.y);
+    draw_debug_overlay(&hw_state->video, gui, ctx->game);
     hw_vbe_flip_buffer(&hw_state->video);
 }
 
@@ -109,6 +109,7 @@ void handle_mouse(hardware_t *hw_state, t_ctx *ctx) {
     if (is_pressed) {
         if (!*clicked) {
             // --- 1. MOUSE JUST PRESSED ---
+            ctx->game.click_count++;
             *clicked = target;
             if (target) {
                 WIDGET_SET_CLICKED(target, true);
